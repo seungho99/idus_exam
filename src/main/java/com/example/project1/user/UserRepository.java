6d8baff8526f31ac1d1d2c207f407d50e37b1144
seungho99 <shsm66@naver.com>
2025-02-11 17:29:06 +0900

    [Feature] 단일 회원 상세정보 조회 기능 추가, Swagger설정

--- src/main/java/com/example/project1/user/UserRepository.java
+++ src/main/java/com/example/project1/user/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.project1.user;
 import com.example.project1.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String username);
+    Optional<User> findById(Long idx);
+
 }
 
--- src/main/java/com/example/project1/user/model/UserDto.java
+++ src/main/java/com/example/project1/user/model/UserDto.java
@@ -1,14 +1,63 @@
 package com.example.project1.user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class UserDto {
 
+    @AllArgsConstructor
+    @Getter
+    public static class ReadResponse {
+        @Schema(description = "응답 성공 여부", example = "true")
+        private Boolean isSuccess;
+        private UserResponse result;
+
+        public static ReadResponse success(UserResponse result) {
+            return new ReadResponse(true, result);
+        }
+    }
+
+    @Getter
+    public static class UserResponse {
+        @Schema(description = "회원번호", example = "1")
+        private Long idx;
+
+        @Schema(description = "회원 이메일", example = "test01@test.com")
+        private String email;
+
+        @Schema(description = "회원 이름", example = "test01")
+        private String name;
+
+        @Schema(description = "회원 전화번호", example = "010-1234-5678")
+        private String phone;
+
+        @Schema(description = "회원 등급", example = "USER")
+        private String role;
+
+        public static UserResponse from(User user) {
+            UserResponse response = new UserResponse();
+            response.idx = user.getIdx();
+            response.email = user.getEmail();
+            response.name = user.getName();
+            response.phone = user.getPhone();
+            response.role = user.getRole();
+            return response;
+        }
+    }
+
     @Getter
     public static class SignupRequest {
+        @Schema(description = "사용자 이메일", example = "test01@test.com")
         private String email;
+
+        @Schema(description = "사용자 비밀번호", example = "qwer1234")
         private String password;
+
+        @Schema(description = "사용자 이름", example = "test01")
         private String name;
+
+        @Schema(description = "사용자 전화번호", example = "010-1234-5678")
         private String phone;
 
         public User toEntity(String encodedPassword, String type) {
